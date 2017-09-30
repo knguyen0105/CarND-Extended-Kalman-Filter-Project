125cecce8b68d0f48dcc3a099e2e4771531d73a4
Khanh Nguyen <nguyen.h.khanh@gmail.com>
2017-09-30 09:29:03 +0700

    done tools

--- src/tools.cpp
+++ src/tools.cpp
@@ -15,9 +15,18 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   TODO:
     * Calculate the RMSE here.
   */
-  VectorXd rmse; 
+  VectorXd rmse(4); 
+  rmse << 0,0,0,0;
 
-  for (int i=0; i < estimations.size(); i++) {
+  if (estimations.size() != ground_truth.size() 
+        || estimations.size() == 0) 
+  {
+    std::cout << "invalid estimation or ground truth data" << endl; 
+    return rmse;          
+  }
+
+  for (int i=0; i < estimations.size(); i++) 
+  {
     rmse += (estimations[i] - ground_truth[i]).cwiseProduct(estimations[i] - ground_truth[i]);
   }
 
@@ -31,4 +40,24 @@ MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
   TODO:
     * Calculate a Jacobian here.
   */
+
+  MatrixXd H(3,4);
+  double px = x_state(0);
+  double py = x_state(1);
+  double vx = x_state(2);
+  double vy = x_state(3);
+  
+  double c1 = px*px + py*py;
+  double c2 = std::sqrt(c1);
+  double c3 = c1 * c2;
+
+  if (std::fabs(c1) < 0.0001) {
+    std::cout << "CalculateJacobian () -Error Division by Zero" << endl;
+    return H;
+  }
+
+  H << px/c2, py/c2, 0, 0,
+       -py/c1, px/c1, 0, 0,
+       py*(vx*py - vy*px)/c3, px*(vy*px - vx*py)/c3, px/c2, py/c2;
+  return H;
 }
